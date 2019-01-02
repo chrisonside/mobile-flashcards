698393c3858fcfa6bfdc171d33308fa496b6f865
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-02 17:49:36 +0000

    Add basic reducers and actions to allow early dev work

---
+++ actions/index.js
@@ -0,0 +1,25 @@
+export const GET_DECK_DATA = 'GET_DECK_DATA';
+export const ADD_DECK = 'ADD_DECK';
+export const ADD_CARD = 'ADD_CARD';
+
+export function getDeckData () {
+  const dummyData = {};
+  return {
+    type: GET_DECK_DATA,
+    dummyData,
+  }
+}
+
+export function addDeck (deck) {
+  return {
+    type: ADD_DECK,
+    deck,
+  }
+}
+
+export function addCard (card) {
+  return {
+    type: ADD_CARD,
+    card,
+  }
+}
---
+++ reducers/index.js
@@ -0,0 +1,14 @@
+import {
+  GET_DECK_DATA,
+  ADD_DECK,
+  ADD_CARD
+} from '../actions';
+
+function deckData (state = {}, action) {
+  switch (action.type) {
+    default :
+      return state
+  }
+}
+
+export default deckData;
