b8802f4f34973c4a8122a5f3b2ce61cfe8fbee6c
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-02 10:57:14 +0000

    Add color scheme from Adobe Kuler

---
+++ utils/colors.js
@@ -0,0 +1,7 @@
+export const white = '#fff'
+export const black = '#000'
+export const red = '#BF2431';
+export const navy = '#1D2059';
+export const blue = '#08678C';
+export const orange = '#F28322';
+export const offWhite = '#F2F2F2';
