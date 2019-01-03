442dbcc95bae35e75244b26a3163ba2bad80c42b
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-03 00:37:56 +0000

    Add logic for switching screens on Quiz component and tallying score

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,37 +1,99 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import ScoreCard from './components/ScoreCard';
-import Pagination from './components/Pagination';
-import StyledButton from './components/StyledButton';
+import ScoreCard from './ScoreCard';
+import Pagination from './Pagination';
+import StyledButton from './StyledButton';
 import { black, white } from '../utils/helpers';
 
 export default class Quiz extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentQuestionNum: 1,
+      correctAnswers: 0,
+      showQuestion: true,
+    }
+  }
 
-    const currentQuestion = {
-      title: 'What is my name',
-      answer: 'blah',
-      questionNumber: 1,
+  // todo - add ability to restart the quiz
+  restartQuiz = () => {
+  }
+
+  flipCard = () => {
+    this.setState(prevState => ({
+      showQuestion: !prevState.showQuestion
+    }));
+  }
+
+  handleCorrectAnswer = () => {
+    this.setState(prevState => ({
+      currentQuestionNum: prevState.currentQuestionNum + 1,
+      correctAnswers: prevState.correctAnswers + 1,
+      showQuestion: true,
+    }));
+  }
+
+  handleIncorrectAnswer = () => {
+    this.setState(prevState => ({
+      currentQuestionNum: prevState.currentQuestionNum + 1,
+      showQuestion: true,
+    }));
+  }
+
+  render() {
+    const { currentQuestionNum, correctAnswers, showQuestion } = this.state;
+
+    // todo - replace this with real data
+    const currentDeckCards = [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      },
+      {
+        question: 'What is my name?',
+        answer: 'Chris'
+      },
+    ];
+
+    const totalQuestions = currentDeckCards.length;
+
+    const currentCard = currentDeckCards[currentQuestionNum - 1];
+
+    // Display scorecard if all cards in deck answered
+    // I pass navigation as prop so that the functional component ScoreCard can access it
+    // todo - style this properly
+    if (currentQuestionNum > totalQuestions) {
+      return (
+        <View>
+          <ScoreCard navigation={this.props.navigation} score={correctAnswers} total={totalQuestions}></ScoreCard>
+        </View>
+      )
     }
 
     return (
       <View>
-        <View>
-          <Pagination page={currentQuestion.questionNumber}></Pagination>
-          <Text>{currentQuestion.title}</Text>
-          <Text>Show answer</Text>
-          <StyledButton onPress={console.log('Correct pressed')}>Correct</StyledButton>
-          <StyledButton onPress={console.log('Incorrect pressed')}>Incorrect</StyledButton>
-        </View>
-        <View>
-          <Pagination page={currentQuestion.questionNumber}></Pagination>
-          <Text>{currentQuestion.answer}</Text>
-          <Text>Show question</Text>
-          <StyledButton onPress={console.log('Correct pressed')}>Correct</StyledButton>
-          <StyledButton onPress={console.log('Incorrect pressed')}>Incorrect</StyledButton>
-        </View>
-        <ScoreCard score={2} total={4}></ScoreCard>
+        <Pagination currentNum={currentQuestionNum} total={totalQuestions}></Pagination>
+        {(showQuestion) && (
+          <View>
+            <Text>{currentCard.question}</Text>
+            <Text onPress={this.flipCard}>Show answer</Text>
+            <StyledButton onPress={this.handleCorrectAnswer}>Correct</StyledButton>
+            <StyledButton onPress={this.handleIncorrectAnswer}>Incorrect</StyledButton>
+          </View>
+        )}
+        {(!showQuestion) && (
+          <View>
+            <Text>{currentCard.answer}</Text>
+            <Text onPress={this.flipCard}>Show question</Text>
+            <StyledButton onPress={this.handleCorrectAnswer}>Correct</StyledButton>
+            <StyledButton onPress={this.handleIncorrectAnswer}>Incorrect</StyledButton>
+          </View>
+        )}
       </View>
     )
   }
