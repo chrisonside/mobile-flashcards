cbbea54058616ad53be153bde25b687fa6276a5a
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-03 00:34:58 +0000

    Handle TextInput for AddQuizCard component

--- components/AddQuizCard.js
+++ components/AddQuizCard.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import StyledButton from './StyledButton';
 import { black, white } from '../utils/helpers';
 
 export default class AddQuizCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: '',
+      answer: '',
+    };
+  }
+
+  handleSubmit = () =>  {
+    const question = this.state.question;
+    const answer = this.state.answer;
+    // todo - Add to AsyncStorage, our 'database' for this project
+    // todo - Update Redux store - addCard()
+    // Finally switch screens
+    this.props.navigation.navigate('Deck');
+  }
+
   render() {
     return (
       <View>
-        <Text>Form input for question - need correct tag</Text>
-        <Text>Form input for answer - need correct tag</Text>
-        <StyledButton onPress={console.log('Submit pressed')}>Submit</StyledButton>
+        <TextInput
+          placeholder='Question'
+          onChangeText={(question) => this.setState({question})}
+        />
+        <TextInput
+          placeholder='Answer'
+          onChangeText={(answer) => this.setState({answer})}
+        />
+        <StyledButton onPress={this.handleSubmit}>Submit</StyledButton>
       </View>
     )
   }
