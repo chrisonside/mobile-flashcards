57f707c02131497a476d7e85c13f4b52da2c663a
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-03 00:34:11 +0000

    Add TextInput for AddDeck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import StyledButton from './StyledButton';
 import { black, white } from '../utils/helpers';
 
 export default class AddDeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: ''
+    };
+  }
+
+  handleSubmit = () =>  {
+    const textToSubmit = this.state.inputText;
+    // todo - Add to AsyncStorage, our 'database' for this project
+    // todo - Update Redux store - addDeck() and setCurrentDeck()
+
+    // Finally switch screens
+    this.props.navigation.navigate('Deck');
+  }
+
+  // todo - disable submit button unless this.state.inputText is populated
   render() {
     return (
       <View>
         <Text>What is the title of your new deck?</Text>
-        <Text>Form input goes here - need correct tag</Text>
-        <StyledButton onPress={console.log('Submit pressed')}>Submit</StyledButton>
+        <TextInput
+          placeholder='Deck title'
+          onChangeText={(inputText) => this.setState({inputText})}
+          onSubmitEditing={this.handleSubmit}
+        />
+        <StyledButton onPress={this.handleSubmit}>Submit</StyledButton>
       </View>
     )
   }
