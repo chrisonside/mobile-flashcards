f0025676287e113a5aaa4036dbcbac5f1bbbb611
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-04 19:18:35 +0000

    Add helper functions for dealing with objects and arrays

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,24 @@
 export function isArrayEmpty(array) {
   return array.length <= 0;
 }
+
+/* Note - I am re-using 3 helper functions from a previous Udacity project */
+export function isObjectEmpty(obj) {
+  if(Object.keys(obj).length === 0 && obj.constructor === Object) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function convertToArray(object) {
+  return Object.keys(object).map(i => object[i]);
+}
+
+export function objectToArray(object) {
+  let myArray = [];
+  if(!isObjectEmpty(object)) {
+    myArray = convertToArray(object);
+  }
+  return myArray;
+}
