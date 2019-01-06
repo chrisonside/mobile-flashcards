93eb6ae5f44273328b25501aedace3371fbfa643
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-06 00:50:35 +0000

    Clear inputs on submit and tweak button styles

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
 import { addDeck, setCurrentDeck } from '../actions';
-import StyledButton from './StyledButton';
-import { black, white } from '../utils/colors';
+import { blue, green, white } from '../utils/colors';
 
 class AddDeck extends Component {
   constructor(props) {
@@ -24,27 +24,79 @@ class AddDeck extends Component {
     this.props.dispatch(addDeck(deckKey, deckData));
     this.props.dispatch(setCurrentDeck(deckKey));
 
-    // Finally switch screens
-    this.props.navigation.navigate('Deck');
+    // Finally clear input text and switch screens
+    this.setState({ inputText: ''}, function() {
+      this.props.navigation.navigate('Deck');
+    });
   }
 
   render() {
     const shouldSubmitBeDisabled = !this.state.inputText;
+    const { inputText } = this.state;
     return (
-      <View>
-        <Text>What is the title of your new deck?</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>What is the title of your new deck?</Text>
         <TextInput
+          style={styles.inputText}
+          selectionColor={blue}
           placeholder='Deck title'
           onChangeText={(inputText) => this.setState({inputText})}
+          value={inputText}
         />
         <TouchableOpacity
+          style={Platform.OS === 'ios' ? styles.iosSubmit : styles.AndroidSubmit}
           onPress={this.handleSubmit}
           disabled={shouldSubmitBeDisabled}>
-            <Text>Submit</Text>
+            <Text style={styles.submitText}>Create Deck</Text>
         </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  inputText: {
+    alignSelf: 'stretch',
+    height: 40,
+    fontSize: 20,
+    marginBottom: 15,
+    paddingLeft: 5,
+    paddingRight: 5,
+    textAlign: 'center',
+  },
+  iosSubmit: {
+    height: 45,
+    padding: 10,
+    borderRadius: 6,
+    backgroundColor: green,
+    alignSelf: 'stretch',
+  },
+  AndroidSubmit: {
+    height: 45,
+    paddingTop: 10,
+    paddingRight: 25,
+    paddingBottom: 13,
+    paddingLeft: 25,
+    borderRadius: 3,
+    backgroundColor: green,
+    alignSelf: 'stretch',
+  },
+  submitText: {
+    color: white,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
 
 export default connect()(AddDeck);
--- components/AddQuizCard.js
+++ components/AddQuizCard.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux';
 import { Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { addCard, addCardToCurrentDeck } from '../actions';
-import StyledButton from './StyledButton';
 import { objectToArray } from '../utils/helpers';
-import { black, white } from '../utils/helpers';
+import { blue, green, white } from '../utils/colors';
 
 class AddQuizCard extends Component {
   constructor(props) {
@@ -23,30 +22,87 @@ class AddQuizCard extends Component {
     cardObject['answer'] = this.state.answer;
     this.props.dispatch(addCard(currentDeckKey, cardObject));
     this.props.dispatch(addCardToCurrentDeck(currentDeckKey, cardObject));
-    this.props.navigation.navigate('Deck');
+
+    this.setState({question: '', answer: ''}, function() {
+      this.props.navigation.navigate('Deck');
+    });
   }
 
   render() {
+    const { question, answer } = this.state;
     const shouldSubmitBeDisabled = (!this.state.question || !this.state.answer);
     return (
-      <View>
+      <View style={styles.container}>
         <TextInput
+          style={styles.inputText}
+          selectionColor={blue}
           placeholder='Question'
           onChangeText={(question) => this.setState({question})}
+          value={question}
         />
         <TextInput
+          style={styles.inputText}
+          selectionColor={blue}
           placeholder='Answer'
           onChangeText={(answer) => this.setState({answer})}
+          value={answer}
         />
         <TouchableOpacity
+          style={Platform.OS === 'ios' ? styles.iosSubmit : styles.AndroidSubmit}
           onPress={this.handleSubmit}
           disabled={shouldSubmitBeDisabled}>
-            <Text>Submit</Text>
+            <Text style={styles.submitText}>Submit</Text>
         </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  inputText: {
+    height: 40,
+    fontSize: 20,
+    marginBottom: 15,
+    paddingLeft: 5,
+    paddingRight: 5,
+    textAlign: 'center',
+    alignSelf: 'stretch',
+  },
+  iosSubmit: {
+    height: 45,
+    padding: 10,
+    borderRadius: 6,
+    backgroundColor: green,
+    alignSelf: 'stretch',
+  },
+  AndroidSubmit: {
+    height: 45,
+    padding: 10,
+    paddingRight: 25,
+    paddingLeft: 25,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: green,
+    alignSelf: 'stretch',
+  },
+  submitText: {
+    color: white,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
 
 function mapStateToProps (state) {
   const currentDeckKey = state.currentDeck.title;
