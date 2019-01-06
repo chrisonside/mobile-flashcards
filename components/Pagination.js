164a1bd101d21813ec86cbf66403d8c02dc4e61c
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-06 00:48:13 +0000

    Style Pagination component

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { black, white } from '../utils/helpers';
+import { blue } from '../utils/colors';
 
 export default function Pagination ({ currentNum, total }) {
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-        <Text>{currentNum}/{total}</Text>
+        <Text style={styles.title}>Question {currentNum} out of {total}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+    padding: 0,
+  },
+  title: {
+    marginBottom: 10,
+    fontSize: 18,
+    fontWeight: '700',
+    color: blue,
+  },
+});
