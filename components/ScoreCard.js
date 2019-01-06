8e420a5806488c9052eb0948ed22441231b4d81d
Chris Reidy <christopher.t.reidy@gmail.com>
2019-01-06 00:49:45 +0000

    Style Scorecard component

--- components/ScoreCard.js
+++ components/ScoreCard.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import StyledButton from './StyledButton';
-import { black, white } from '../utils/helpers';
+import { blue, green } from '../utils/colors';
 
 export default function ScoreCard ({ navigation, score, total }) {
 
   return (
     <View>
-      <View>
-        <Text>You got {score} correct!</Text>
-        <StyledButton onPress={() => {navigation.navigate('Quiz')}}>Start quiz again</StyledButton>
-        <StyledButton onPress={() => {navigation.navigate('Deck')}}>Back to deck</StyledButton>
+      <View style={styles.container}>
+        <Text style={styles.title}>You got {score} correct!</Text>
+        <StyledButton style={{ backgroundColor: green, marginBottom: 20 }} onPress={() => {navigation.navigate('Quiz')}}>Start quiz again</StyledButton>
+        <StyledButton style={{ backgroundColor: blue, marginBottom: 20 }} onPress={() => {navigation.navigate('Deck')}}>Back to deck</StyledButton>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    alignItems: 'stretch',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+});
